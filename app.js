56e57e88419362beca6b8ea1676fea7150b66b17
orangemug <info@orangemug.co.uk>
2014-08-01 13:36:51 +0100

    Fix ugly markup/code

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-var sEl = document.querySelector(".screen-size");
-var pEl = document.querySelector(".page-size");
+window.onload = function() {
+  var sEl = document.querySelector(".screen-size");
+  var pEl = document.querySelector(".page-size");
 
-document.body.addEventListener("mousemove", function(e) {
-  var px = e.pageX;
-  var py = e.pageY;
-  var sx = e.screenX;
-  var sy = e.screenY;
+  document.body.addEventListener("mousemove", function(e) {
+    var px = e.pageX;
+    var py = e.pageY;
+    var sx = e.screenX;
+    var sy = e.screenY;
 
-  sEl.innerHTML = sx+" x "+sy;
-  pEl.innerHTML = px+" x "+py;
-}, false);
+    sEl.innerHTML = sx+" x "+sy;
+    pEl.innerHTML = px+" x "+py;
+  }, false);
+};
